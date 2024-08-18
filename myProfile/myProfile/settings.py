2e844c95f1ea2d171d6ce833b9dd160aab550737
Pumelela-Banca <pumelelabanca@gmail.com>
2024-08-19 00:45:40 +0200

    removed load static

--- myProfile/myProfile/settings.py
+++ myProfile/myProfile/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ies$mob8obbt_cb2+xj4jfiv-0+s!%hmmo@pc)fj05oxrv&qot
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
