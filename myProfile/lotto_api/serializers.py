826b9fe3815e498886625376de9ec8241c69578c
Pumelela-Banca <pumelelabanca@gmail.com>
2024-08-22 18:14:53 +0200

    added serielizer for all  draws

--- myProfile/lotto_api/serializers.py
+++ myProfile/lotto_api/serializers.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
+from .models import *
 
 
-class LottoSerializer(serializers.Serializer):
-    
-
+class LottoSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = '__all__'
         model = LottoP1
-        
+        fields = '__all__'
+
+
+class LottoSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = LottoP2
+        fields = '__all__'
+
+
+class LottoSerializer3(serializers.ModelSerializer):
+    class Meta:
+        model = LottoP3
+        fields = '__all__'
+
+
+class PowerBallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PowerBall
+        fields = '__all__'
+
+
+class PowerBallPlusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PowerBallP1
+        fields = '__all__'
+
+
+class DailyLottoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Daily
+        fields = '__all__'
