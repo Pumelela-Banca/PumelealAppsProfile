c78393f80b3e325d8de795e2f806c274b3aa8560
Pumelela-Banca <pumelelabanca@gmail.com>
2025-06-25 14:26:42 +0200

    added icon for browsers

--- myProfile/mainPage/admin.py
+++ myProfile/mainPage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Projects, Services
+from .models import Projects, Services, ContactUs
 
 # Register your models here.
 
 admin.site.register(Projects)
 admin.site.register(Services)
+admin.site.register(ContactUs)
